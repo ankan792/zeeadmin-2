d6ff5abce43b389a5474fdd5602adfab90938988
S H E R L O C K <97678993+SHERLOCKx90@users.noreply.github.com>
2023-06-10 18:38:08 +0530

    initial project

--- server.js
+++ server.js
@@ -3,9 +3,14 @@ const app = express()
 const port = 3000
 
 
-app.get('/',(request, response)=>{
+app.get('/', (request, response) => {
     response.send("Hello World")
 })
+
+app.get('/blog', (request, response) => {
+    response.send("Welcome to SHERLOCK's blog")
+})
+
 app.listen(port, () => {
     console.log(`Server is active on: ${port}`)
 })
