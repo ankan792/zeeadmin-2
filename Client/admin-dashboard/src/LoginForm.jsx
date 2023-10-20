aa6bf15b963b73e454c8291b2278f453df0c0c02
Sujay Ghosh <sujayghosh@Sujays-MacBook-Air.local>
2023-10-20 17:15:30 +0530

    Sujay's Changes

--- Client/admin-dashboard/src/LoginForm.jsx
+++ Client/admin-dashboard/src/LoginForm.jsx
@@ -17,28 +17,30 @@ const Login = ({ onLogin }) => {
 
   return (
     <div className="bg-red-100 min-h-screen flex justify-center items-center">
-      <div className="bg-white p-16 rounded-[30px] shadow-md w-[40%]"> {/* Adjust width and padding */}
-        <h1 className="text-3xl text-center text-red-800 font-bold mb-6">
+      <div className="bg-white p-4 md:p-16 rounded-[30px] shadow-md w-full md:w-[40%] max-w-md">
+        <h1 className="text-3xl md:text-4xl text-center text-red-800 font-bold mb-4 md:mb-6">
           Zee Bangla Mukto Mancho Admin Dashboard
         </h1>
-        <h2 className="text-2xl text-black mt-10 font-medium mb-6">Login</h2> {/* Increase font size */}
+        <h2 className="text-xl md:text-2xl text-black mt-6 md:mt-10 font-medium mb-2 md:mb-6">
+          Login
+        </h2>
         <input
           type="text"
           placeholder="Username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
-          className="block w-full p-4 border rounded-[15px] mb-6 hover:border-red-500 active:border-red-700 focus:outline-red-700"  // Adjust padding
+          className="block w-full p-3 md:p-4 border rounded-[15px] mb-2 md:mb-6 hover:border-red-500 active:border-red-700 focus:outline-red-700"
         />
         <input
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          className="block w-full p-4 border rounded-[15px] mb-6 hover:border-red-500 active:border-red-700 focus:outline-red-700" // Adjust padding
+          className="block w-full p-3 md:p-4 border rounded-[15px] mb-4 md:mb-6 hover:border-red-500 active:border-red-700 focus:outline-red-700"
         />
         <button
           onClick={handleLogin}
-          className="bg-red-500 text-white text-2xl p-4 rounded-[20px] w-full font-semibold hover:bg-red-800" // Adjust padding
+          className="bg-red-500 text-white text-xl md:text-2xl p-3 md:p-4 rounded-[20px] w-full font-semibold hover:bg-red-800"
         >
           Sign In
         </button>
